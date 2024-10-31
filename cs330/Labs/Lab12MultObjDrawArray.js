ebb27335612f56ddeded28b390dd968870ef79b7
unknown <shinycup51@gmail.com>
2024-10-31 11:11:57 -0400

    labs

--- cs330/Labs/Lab12MultObjDrawArray.js
+++ cs330/Labs/Lab12MultObjDrawArray.js
@@ -124,15 +124,15 @@ function render(){
 
     projectionMatrix = perspective(fovy, aspect, near, far);
     var S = scale(0.5,0.5,0.5);
-    var Tleft = translate(-1.0,0,0);
     var Tright = translate(1.0,0,0);
+    var Tleft = translate(-1.0,0,0);
 
     // Cube on the left
     // Scale is "first," since it's closest to the vertex, then translate
     // since it is left multiplied
     modelViewMatrix = lookAt(eye, at , up);
-    // update modelview matrix with required transformation(s)
-
+    modelViewMatrix = mult(modelViewMatrix,Tleft);
+    modelViewMatrix = mult(modelViewMatrix,S);
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
     gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
     gl.drawArrays(gl.TRIANGLES, 0, numPositions);
@@ -140,8 +140,7 @@ function render(){
     // Cube in the middle
     // just need to Scale, no translate, coord are already centered
     modelViewMatrix = lookAt(eye, at , up);
-    // update modelview matrix with required transformation(s)
-
+    modelViewMatrix = mult(modelViewMatrix,S);
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
     gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
     gl.drawArrays(gl.TRIANGLES, 0, numPositions);
@@ -150,8 +149,8 @@ function render(){
     // Scale is "first," since it's closest to the vertex, then translate
     // since it is left multiplied
     modelViewMatrix = lookAt(eye, at , up);
-    // update modelview matrix with required transformation(s)
-
+    modelViewMatrix = mult(modelViewMatrix,Tright);
+    modelViewMatrix = mult(modelViewMatrix,S);
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
     gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
     gl.drawArrays(gl.TRIANGLES, 0, numPositions);
